2571f64819aebfa8b3131f3c7538f57b1a80d50a
Scott Kuhl <kuhl@mtu.edu>
2017-04-11 20:25:54 -0400

    switch to rfid reader

--- swipe.py
+++ swipe.py
@@ -10,6 +10,8 @@ import time
 # Read card without echoing input to terminal
 from getpass import getpass
 
+filenameDB = "swipe-db.csv"
+
 
 def playSoundFile(filename):
     """Try to play an audio file"""
@@ -47,56 +49,50 @@ def soundEnterName():
     playSoundFile("swipe-boing.wav")
         
 
-def hasNumbers(inputString):
-    """Return true if string contains one or more digits"""
-    return any(char.isdigit() for char in inputString)
-
 def validCard(card):
     """Check if a card is 'valid'. If it is, return a hash of the card (we hash the card ourselves so we don't have to worry about storing whatever data is actually on the card). If the card is not valid, return None"""
     card = card.strip()
     #print("Card: "+card)
     #print("Card in hex: " + ":".join("{:02x}".format(ord(c)) for c in card))
     
-    if re.fullmatch(";.{16}=.{20}\?", card):
+    if re.fullmatch("[0-9]{10}", card):
         return hashlib.sha1(card.encode("ascii")).hexdigest()
     else:
         return None
 
-def validName(name):
+def validUsername(name):
     """Check if the name that the user typed in was valid. We apply this
 check so that a person swiping their card two times in a row doesn't
 have their card show up as their name accidentally."""
     if validCard(name):
         return False
-    if ',' in name or "?" in name or "=" in name or ";" in name or "/" in name or "^" in name:
+    if ',' in name or "?" in name or "=" in name or ";" in name or "/" in name or "^" in name or '"' in name or '@' in name:
         return False
     if len(name) < 3:
         return False
-    if " " not in name:
-        return False
-    if hasNumbers(name):
+    if " " in name:
         return False
     
     return True
 
 
 def waitForCard(attempts=0):
-    """Prompt the user to swipe their card until we see a valid card."""
+    """Prompt the user to wave their card and wait until we see a valid card."""
     if attempts == 0:
         for i in range(5):
             print()
 
     print("💳") # unicode card emoji
-    card = getpass(prompt="Swipe card now:")
+    card = getpass(prompt="Wave card now:")
     card = card.strip()
 
-    # On Linux, there is an issue where the first card swipe works
-    # fine, then the second card swipe causes getpass() to only return
+    # On Linux, there is an issue where the first card read works
+    # fine, then the second card read causes getpass() to only return
     # part of the card. Here, we wait until the correct number of
     # characters are present in the input. Note that if someone types
-    # something on the keyboard and then swipes the card, that swipe
-    # won't work. The next swipe should work, however.
-    while len(card) < 39:
+    # something on the keyboard and then uses the card, it won't work.
+    # The next time the card is used, it should work.
+    while len(card) < 10:
         card = card + getpass(prompt="")
         card = card.strip()
 
@@ -107,11 +103,11 @@ def waitForCard(attempts=0):
     # Drop the raw data of the card, we don't need it and don't want
     # it anymore.
     del card
-    
+
 
     if hashed == None:
         print("🚫") # prohibited emoji
-        print("ERROR: Invalid card. Swipe again. Use gold magnetic strip.")
+        print("ERROR: Invalid card. Try again.")
         soundError()
         return waitForCard(attempts+1)
 
@@ -124,16 +120,17 @@ def waitForName():
     soundEnterName()
 
     name = ""
-    while not validName(name):
+    while not validUsername(name):
         name = ""
         print("🤔")
-        print("Type name (of person who last swiped their card): ")
+        print("Type the username of the person who last swiped their card: ")
         while len(name) == 0:
             name = sys.stdin.readline()
             name = name.strip()
-        if not validName(name):
+        name = name.lower()  # lowercase the username
+        if not validUsername(name):
             print("🚫") # prohibited emoji
-            print("ERROR: Enter a valid first and last name (separated by a space). No punctuation or numbers.")
+            print("ERROR: Enter a valid username. No punctuation.")
             soundError()
 
 
@@ -141,7 +138,7 @@ def waitForName():
 
 def writeDB(db):
     """Write a dictionary to our database file."""
-    with open("swipe-db.csv", "w") as csvfile:
+    with open(filenameDB, "w") as csvfile:
         writer = csv.writer(csvfile)
         for k, v in db.items():
             writer.writerow([v, k])
@@ -149,10 +146,10 @@ def writeDB(db):
 
 def readDB():
     """Read our database file to construct a dictionary. The hashes of the cards are the keys and the person's name is the value."""
-    if not os.path.exists("swipe-db.csv"):
+    if not os.path.exists(filenameDB):
         return { }
     
-    with open("swipe-db.csv", "r") as csvfile:
+    with open(filenameDB, "r") as csvfile:
         rows = csv.reader(csvfile)
         if rows:
             db = { }
@@ -164,7 +161,7 @@ def readDB():
 
 
 db = readDB()
-print("%d cards+names in swipe-db.csv" % len(db))
+print("%d cards+names in %s" % (len(db), filenameDB))
 
 outputFilename = time.strftime('swipe-attend-%Y%m%d.csv')
 if os.path.exists(outputFilename):
